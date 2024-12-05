823935113423895054c33109d5c0f745c1fe2f89
Wimpert <holvoetwim@gmail.com>
2024-12-05 10:27:58 +0100

    function of generator

--- libs/internal-plugin/src/generators/update-scope-schema/generator.ts
+++ libs/internal-plugin/src/generators/update-scope-schema/generator.ts
@@ -1,15 +1,17 @@
-import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  Tree,
-  updateJson,
-} from '@nx/devkit';
+import { formatFiles, Tree, updateJson } from '@nx/devkit';
+import { UpdateScopeSchemaGeneratorSchema } from './schema';
 
-export default async function (tree: Tree) {
+export async function updateScopeSchemaGenerator(
+  tree: Tree,
+  options: UpdateScopeSchemaGeneratorSchema
+) {
+  console.log('Updating scope schema...');
+  console.log('options:', options);
   updateJson(tree, 'nx.json', (json) => ({
     ...json,
     defaultProject: 'movies-app',
   }));
   await formatFiles(tree);
 }
+
+export default updateScopeSchemaGenerator;
